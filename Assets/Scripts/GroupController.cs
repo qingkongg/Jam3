021a3543851e73e2a9eff895d7977e5bb8a68af6
青空 <113504120+qingkongg@users.noreply.github.com>
2024-07-16 01:49:02 +0800

    节点1

--- Assets/Scripts/GroupController.cs
+++ Assets/Scripts/GroupController.cs
@@ -31,4 +31,46 @@ public class GroupController : MonoBehaviour
             transform.Rotate(0, 0, -90);
         }
     }
+
+    void UpdateGrid()
+    {
+        for (int i = 0; i < GameController.colNum; i++)
+        {
+            for(int j = 0; j < GameController.colNum; j++)
+            {
+                if (GameController.Grid[j,i] != null)
+                {
+                    if(GameController.Grid[j, i].parent == gameObject.transform)
+                    {
+                        GameController.Grid[j, i] = null;
+                    }
+                }
+            }
+        }
+        foreach(Transform child in transform)
+        {
+            Vector2 v = GameController.RoundVec2 (child.position);
+            GameController.Grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+
+    bool IsValidPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 pos = child.position;
+            if (!GameController.IsInside(pos))
+            {
+                return false;
+            }
+            else if (GameController.IsInside(pos))
+            {
+                if (GameController.Grid[(int)pos.x, (int)pos.y] != null && GameController.Grid[(int)pos.x, (int)pos.y].parent != gameObject.transform)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
