8d561c271845718c134b8aebebfe3849bedaa5b7
青空 <113504120+qingkongg@users.noreply.github.com>
2024-07-17 23:25:17 +0800

    slow

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -28,7 +28,7 @@ public class GameController : MonoBehaviour
     public float FallingPositin_Y = 0;
 
     //�������
-    public static Transform[,] Grid ;
+    public static Transform[,] Grid;
 
     public static ColorState[,] GameManager;
     public static bool[,] CancelManager;
@@ -67,7 +67,7 @@ public class GameController : MonoBehaviour
         {
             for (int j = 0; j < colNum; j++)
             {
-                GameManager[i,j] = ColorState.None; 
+                GameManager[i, j] = ColorState.None;
             }
         }
 
@@ -77,17 +77,17 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		
+
         //��һ���Ѿ����
         if (Isfallen)
         {
             m_timer += Time.deltaTime;
-            if(m_timer >= CD)//����CD
+            if (m_timer >= CD)//����CD
             {
                 RandomGenerateBlock();
                 Debug.Log("x");
-                Isfallen =false;
-                m_timer=0;
+                Isfallen = false;
+                m_timer = 0;
             }
         }
 
@@ -97,30 +97,30 @@ public class GameController : MonoBehaviour
 
     private void RandomGenerateBlock()
     {
-        int index = Random.Range(0,FallingBlocks.Length);
-        Instantiate(FallingBlocks[index],new Vector3(X_Offset+FallingPositin_X,Y_Offset+FallingPositin_Y,0),Quaternion.identity);
+        int index = Random.Range(0, FallingBlocks.Length);
+        Instantiate(FallingBlocks[index], new Vector3(X_Offset + FallingPositin_X, Y_Offset + FallingPositin_Y, 0), Quaternion.identity);
 
     }
 
     public static Vector2 RoundVec2(Vector2 v)
     {
         //Debug.Log("From" + v + "to" + Mathf.Round(v.x) + " " + Mathf.Round(v.y));
-        return new Vector2(Mathf.Round(v.x),Mathf.Round(v.y));
+        return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
     }
 
     public static bool IsInside(Vector2 v)
     {
-        return ((int)v.x >= X_Offset && (int)v.x <= X_Offset + rowNum - 1  && (int)v.y >= Y_Offset );
+        return ((int)v.x >= X_Offset && (int)v.x <= X_Offset + rowNum - 1 && (int)v.y >= Y_Offset);
     }
 
     //如果下方为空，则往下边走
     private void updateColor()
     {
-        for(int i = 0; i < rowNum; i++)
+        for (int i = 0; i < rowNum; i++)
         {
-            for (int j = 1;j < colNum; j++)
+            for (int j = 1; j < colNum; j++)
             {
-                if (Grid[i,j-1] == null && Grid[i,j] != null)
+                if (Grid[i, j - 1] == null && Grid[i, j] != null)
                 {
                     int x = 1;
                     while (j - x - 1 >= 0)
@@ -134,14 +134,10 @@ public class GameController : MonoBehaviour
                             break;
                         }
                     }
-                    Debug.Log("(" + i + "," + j + "move to" + "(" + i + "," + (j-x));
-                    Grid[i, j- x] = Grid[i, j];
-                    Vector3 newposition = Grid[i, j- x].position + Vector3.down * x;
-                    Grid[i,j- x].position = newposition;
-                    Grid[i,j] = null;
-                    GameManager[i, j- x] = GameManager[i, j];
-                    GameManager[i, j] = ColorState.None;
-                    Debug.Log("(" + i + "," + j + "move to" + "(" + i + "," + (j- x));
+                    //Debug.Log("(" + i + "," + j + "move to" + "(" + i + "," + (j - x));
+                    StartCoroutine(MoveBlockDown(i, j, x, Grid[i,j]));
+                    Grid[i, j] = null;
+                    // Debug.Log("(" + i + "," + j + "move to" + "(" + i + "," + (j - x));
                 }
             }
         }
@@ -149,11 +145,11 @@ public class GameController : MonoBehaviour
 
     private void detectClear()
     {
-        for(int i = 0;i < rowNum; i++)
+        for (int i = 0; i < rowNum; i++)
         {
-            for(int j = 0;j < colNum; j++)
+            for (int j = 0; j < colNum; j++)
             {
-                if (GameManager[i,j] != ColorState.None)
+                if (GameManager[i, j] != ColorState.None)
                 {
                     if (detectFive(i, j))
                     {
@@ -163,7 +159,7 @@ public class GameController : MonoBehaviour
                     {
                         Point += PointFour;
                     }
-                    else if(detectThree(i, j))
+                    else if (detectThree(i, j))
                     {
                         Point += PointThree;
                     }
@@ -178,14 +174,14 @@ public class GameController : MonoBehaviour
         {
             for (int j = 0; j < colNum; j++)
             {
-                if (CancelManager[i,j] == true)
+                if (CancelManager[i, j] == true)
                 {
                     //Debug.Log(i + "," + j);
-                    Destroy(Grid[i,j].gameObject);
-                    Grid[i,j] = null ;
+                    Destroy(Grid[i, j].gameObject);
+                    Grid[i, j] = null;
                     //Debug.Log(i + "," + j);
                     GameManager[i, j] = ColorState.None;
-                    CancelManager[i,j] = false;
+                    CancelManager[i, j] = false;
                     m_isClear = true;
                 }
             }
@@ -194,7 +190,7 @@ public class GameController : MonoBehaviour
         {
             Debug.Log("Update");
             updateColor();
-            m_isClear=false;
+            m_isClear = false;
         }
     }
 
@@ -203,15 +199,36 @@ public class GameController : MonoBehaviour
 
     }
 
-    private bool detectThree(int row,int col)
+    private IEnumerator MoveBlockDown(int i, int j, int x,Transform origin)
+    {
+        Transform block = origin;
+        Vector3 targetPosition = block.position + Vector3.down * x;
+        float duration = 0.5f; // 移动的时间
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            block.position = Vector3.Lerp(block.position, targetPosition, (elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+            yield return null; // 等待下一帧
+        }
+
+        block.position = targetPosition; // 确保位置最终精确
+        Grid[i, j - x] = block;
+        GameManager[i, j - x] = GameManager[i, j];
+        GameManager[i, j] = ColorState.None;
+        Debug.Log($"({i},{j}) move to ({i},{j - x})");
+    }
+
+    private bool detectThree(int row, int col)
     {
         if (detectThreeCol(row, col))
         {
             if (detectThreeRow(row, col))
             {
-                CancelManager[row,col] = true;
-                CancelManager[row,col + 1] = true;
-                CancelManager[row,col + 2] = true;
+                CancelManager[row, col] = true;
+                CancelManager[row, col + 1] = true;
+                CancelManager[row, col + 2] = true;
                 Point += PointThree;
             }
             else if (detectThreeRow(row + 1, col))
@@ -221,7 +238,7 @@ public class GameController : MonoBehaviour
                 CancelManager[row + 1, col + 2] = true;
                 Point += PointThree;
             }
-            else if(detectThreeRow(row + 2, col))
+            else if (detectThreeRow(row + 2, col))
             {
                 CancelManager[row + 2, col] = true;
                 CancelManager[row + 2, col + 1] = true;
@@ -234,7 +251,7 @@ public class GameController : MonoBehaviour
             Point += PointThree;
             return true;
         }
-        if(detectThreeRow(row, col))
+        if (detectThreeRow(row, col))
         {
             if (detectThreeCol(row, col))
             {
@@ -265,23 +282,11 @@ public class GameController : MonoBehaviour
         return false;
     }
 
-    private bool detectThreeRow(int row,int col)
+    private bool detectThreeRow(int row, int col)
     {
         if (col + 2 < colNum)
         {
-            if (GameManager[row, col] == GameManager[row, col + 1] && GameManager[row, col] == GameManager[row, col + 2]) 
-            { 
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private bool detectThreeCol(int row,int col)
-    {
-        if (row + 2 < rowNum)
-        {
-            if (GameManager[row, col] == GameManager[row + 1, col] && GameManager[row, col] == GameManager[row + 2, col]) 
+            if (GameManager[row, col] == GameManager[row, col + 1] && GameManager[row, col] == GameManager[row, col + 2])
             {
                 return true;
             }
@@ -289,11 +294,23 @@ public class GameController : MonoBehaviour
         return false;
     }
 
-    private bool detectFour(int row,int col)
+    private bool detectThreeCol(int row, int col)
+    {
+        if (row + 2 < rowNum)
+        {
+            if (GameManager[row, col] == GameManager[row + 1, col] && GameManager[row, col] == GameManager[row + 2, col])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool detectFour(int row, int col)
     {
         if (col + 3 < colNum)
         {
-            if (GameManager[row, col] == GameManager[row, col + 1] && GameManager[row, col] == GameManager[row, col + 2] && GameManager[row, col] == GameManager[row, col + 3] )
+            if (GameManager[row, col] == GameManager[row, col + 1] && GameManager[row, col] == GameManager[row, col + 2] && GameManager[row, col] == GameManager[row, col + 3])
             {
                 Point += 8;
                 CancelManager[row, col] = true;
@@ -318,11 +335,11 @@ public class GameController : MonoBehaviour
         return false;
     }
 
-    private bool detectFive(int row,int col)
+    private bool detectFive(int row, int col)
     {
-        if(col + 4 < colNum)
+        if (col + 4 < colNum)
         {
-            if (GameManager[row,col] == GameManager[row,col+1] && GameManager[row, col] == GameManager[row, col + 2]&&GameManager[row, col] == GameManager[row, col + 3]&&GameManager[row, col] == GameManager[row, col + 4])
+            if (GameManager[row, col] == GameManager[row, col + 1] && GameManager[row, col] == GameManager[row, col + 2] && GameManager[row, col] == GameManager[row, col + 3] && GameManager[row, col] == GameManager[row, col + 4])
             {
                 Point += 20;
                 CancelManager[row, col] = true;
